c06754cd4f5d17c89cc726d107074e89f542639c
eduard villamil <villamilandrey02@gmail.com>
2024-05-28 19:37:40 -0500

    cambio de archivo principal

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-import { app } from "./src/app/router/app.js";
+import express from 'express'
+/* import morgan from 'morgan'; */
+import { router } from './src/app/router/router.js';
+import {resolve} from 'path';
+import { port } from './src/config.js';
+import fileUpload from 'express-fileupload';
+export const app = express();
+const statics = resolve('./src/public');
 
-const port =  app.get("port");
+const PORT = port  || 3100
 
-app.listen(port, ()=>{
-  console.log(`>>>>>>>>>>>>>>>>>>>>>>>    Servidor corriendo por el puerto ${port}  <<<<<<<<<<<<<<<<<<<<<<<<`);
+app.set("views", "./src/public/view");
+app.set("view engine" , "pug");
+
+app.use(fileUpload());
+
+app.use(express.json())
+/* app.use(morgan("dev")); */
+app.use(express.static(statics));
+
+app.use(router)
+
+
+app.listen(PORT, ()=>{
+  console.log(`>>>>>>>>>>>>>>>>>>>>>>>    Servidor corriendo por el puerto ${ PORT }  <<<<<<<<<<<<<<<<<<<<<<<<`);
 });
+ 
+
