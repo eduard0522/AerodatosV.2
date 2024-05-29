4060a0e035982a2bf50ffd746dbd42af78da40ec
eduard villamil <villamilandrey02@gmail.com>
2024-05-28 19:54:21 -0500

    se cambio archivo que exporta el servidor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { router } from './src/app/router/router.js';
 import {resolve} from 'path';
 import { port } from './src/config.js';
 import fileUpload from 'express-fileupload';
-export const app = express();
+const app = express();
 const statics = resolve('./src/public');
 
 const PORT = port  || 3100
@@ -18,11 +18,13 @@ app.use(express.json())
 /* app.use(morgan("dev")); */
 app.use(express.static(statics));
 
-app.use(router)
+app.use('/',router)
 
 
 app.listen(PORT, ()=>{
   console.log(`>>>>>>>>>>>>>>>>>>>>>>>    Servidor corriendo por el puerto ${ PORT }  <<<<<<<<<<<<<<<<<<<<<<<<`);
 });
  
-
+export {
+  app
+}
